7af9adaf8a4a581120674f04ceba48fe54172c0d
Wyatt Evans <syattevans@gmail.com>
2020-07-08 11:07:50 -0500

    Added log to example unit test.

--- app/src/test/java/com/app/googlemapskotlin/ExampleUnitTest.kt
+++ app/src/test/java/com/app/googlemapskotlin/ExampleUnitTest.kt
@@ -5,6 +5,8 @@ import org.junit.Test
 import org.junit.Assert.*
 
 class ExampleUnitTest {
+    private val simpleTag = this::class.java.simpleName
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
